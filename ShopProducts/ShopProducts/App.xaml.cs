c2e2ca14f01b10c9065d457a6f7643e21b0fb668
Stepanov Danil <ds33522777@gmail.com>
2026-02-02 00:21:27 +0300

    Создан родительский проэкт

---
+++ ShopProducts/ShopProducts/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ShopProducts
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Model.HostStepanovContext Context { get; set; } = new Model.HostStepanovContext();
+
+        public static Model.User? CurrentUser { get; set; } = null;
+
+    }
+}
